339ee2e4ed94045f7e15901266cf43498fdfef9a
Eduardo Riboli <eriboli@gmail.com>
2025-12-31 09:37:46 -0300

    Update CORS configuration to allow all origins and standardize methods and headers

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const port = 3000;
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'http://localhost:5173'
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
-
-//const users = [];
 
 app.use('/', route_users);
 
